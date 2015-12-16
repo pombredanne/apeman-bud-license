18fe9e97cdfbe51e6e3380a97e95741e1c5bd59d
okunishinishi <okunishitaka.com@gmail.com>
2015-12-16 09:26:31 +0900

    Better error

--- test/setup_test.js
+++ test/setup_test.js
@@ -8,21 +8,31 @@ const setup = require('../lib/setup.js'),
     assert = require('assert'),
     coz = require('coz');
 
-describe('setup', ()=>{
+describe('setup', ()=> {
 
-it('Setup', (done) => {
-    let bud = setup({
-        type: 'MIT',
-        holder:'foo'
+    it('Setup', (done) => {
+        let bud = setup({
+            type: 'MIT',
+            holder: 'foo'
+        });
+        assert.ok(bud);
+        assert.ok(bud.tmpl);
+        bud.mkdirp = true;
+        bud.path = __dirname + '/../tmp/foo/bar/baz.txt';
+        coz.render(bud, (err) => {
+            assert.ifError(err);
+            done();
+        });
     });
-    assert.ok(bud);
-    assert.ok(bud.tmpl);
-    bud.mkdirp = true;
-    bud.path = __dirname + '/../tmp/foo/bar/baz.txt';
-    coz.render(bud, (err) => {
-        assert.ifError(err);
+
+    it('Setup without type', (done) => {
+        assert.throws(() => {
+            setup({
+                type: null,
+                holder: 'foo'
+            });
+        });
         done();
     });
-});
 });
 
